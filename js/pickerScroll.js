848ed4fb3db8a319fa1c5dfb108c5ce65e950dd4
Changmok LEE <cmlee5075@gmail.com>
2025-06-24 23:43:43 +0900

    feat: wheel picker 기능 추가

---
+++ js/pickerScroll.js
@@ -0,0 +1,16 @@
+const list = document.querySelector(".picker-list");
+const items = list.querySelectorAll("li");
+const picker = document.querySelector(".picker");
+
+picker.addEventListener("scroll", () => {
+  const pickerRect = picker.getBoundingClientRect();
+  const pickerCenter = pickerRect.top + pickerRect.height / 2;
+
+  items.forEach((item) => {
+    const rect = item.getBoundingClientRect();
+    const itemCenter = rect.top + rect.height / 2;
+    const distance = Math.abs(pickerCenter - itemCenter);
+
+    item.classList.toggle("active", distance < 40);
+  });
+});
